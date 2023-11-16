6c09f588af031300742b14e1aebd0b7d792e5eb7
tanlangDark <18130613987@163.com>
2023-11-16 20:40:43 +0800

    -

--- src/main/java/com/example/jsp/mapper/UserMapper.java
+++ src/main/java/com/example/jsp/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.jsp.mapper;
 
 import com.example.jsp.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author tanlang
  * @since 2023-11-16
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
